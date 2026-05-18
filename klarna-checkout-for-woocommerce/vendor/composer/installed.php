6158a9701278086ad1ac56ef7d5f4c41f6e51cf9
Sean Murphy <sean@iamseanmurphy.com>
2026-05-18 07:44:25 +0000

    2.20.2

--- klarna-checkout-for-woocommerce/vendor/composer/installed.php
+++ klarna-checkout-for-woocommerce/vendor/composer/installed.php
@@ -1,9 +1,9 @@
 <?php return array(
     'root' => array(
         'name' => '__root__',
-        'pretty_version' => '2.20.1',
-        'version' => '2.20.1.0',
-        'reference' => '5b6338e43d7be73bb23739fbb67968378d45e011',
+        'pretty_version' => '2.20.2',
+        'version' => '2.20.2.0',
+        'reference' => 'feb7947d6259310ca212ac8f1b9f22e878d68f1a',
         'type' => 'library',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -11,9 +11,9 @@
     ),
     'versions' => array(
         '__root__' => array(
-            'pretty_version' => '2.20.1',
-            'version' => '2.20.1.0',
-            'reference' => '5b6338e43d7be73bb23739fbb67968378d45e011',
+            'pretty_version' => '2.20.2',
+            'version' => '2.20.2.0',
+            'reference' => 'feb7947d6259310ca212ac8f1b9f22e878d68f1a',
             'type' => 'library',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
